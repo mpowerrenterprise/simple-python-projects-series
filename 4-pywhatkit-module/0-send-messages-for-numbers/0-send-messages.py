1b2e10a815d7cadfe8d5ed030552c684982ffcef
Gunarakulan Gunaretnam <45822509+gunarakulangunaretnam@users.noreply.github.com>
2024-04-11 15:03:51 +0530

    docs: cleanup

--- 4-pywhatkit-module/0-send-messages-for-numbers/0-send-messages.py
+++ 4-pywhatkit-module/0-send-messages-for-numbers/0-send-messages.py
@@ -2,8 +2,8 @@ import pywhatkit as pwk
 
 while true:
 
-	pwk.sendwhatmsg("+94740001141", "Good Morning", 8, 00)
+	pwk.sendwhatmsg("+940000000000", "Good Morning", 8, 00)
 
-	pwk.sendwhatmsg("+94740001141", "Good Afternooon", 12, 00)
+	pwk.sendwhatmsg("+940000000000", "Good Afternooon", 12, 00)
 
 	print("Message has been sent")
